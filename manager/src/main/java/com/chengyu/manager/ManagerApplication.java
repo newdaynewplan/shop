5349da8d63eef3e346aaba05932e7a63c8e7d2ee
lenovo <291550492@qq.com>
2018-09-19 21:52:24 +0800

    manager工程加入  @EnableDiscoveryClient 使他成为能被别的工程调用。

--- manager/src/main/java/com/chengyu/manager/ManagerApplication.java
+++ manager/src/main/java/com/chengyu/manager/ManagerApplication.java
@@ -5,6 +5,7 @@ import org.apache.activemq.command.ActiveMQTopic;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -13,6 +14,7 @@ import javax.jms.Queue;
 import javax.jms.Topic;
 
 
+@EnableDiscoveryClient
 @EnableEurekaClient
 @SpringBootApplication
 @MapperScan("com.chengyu.manager.dao")
