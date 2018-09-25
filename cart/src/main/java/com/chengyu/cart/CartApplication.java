83653dd263debf95871793c26332fa54325649ef
jiangchengyu <jiangchengyu@ly-sky.com>
2018-09-25 13:46:44 +0800

    加入断路器监控(Hystrix Dashboard)

--- cart/src/main/java/com/chengyu/cart/CartApplication.java
+++ cart/src/main/java/com/chengyu/cart/CartApplication.java
@@ -3,12 +3,16 @@ package com.chengyu.cart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableHystrix  // 加上@EnableHystrix注解开启断路器
+@EnableHystrixDashboard  // 加上@EnableHystrixDashboard注解，开启HystrixDashboard
+//@EnableCircuitBreaker
 public class CartApplication {
 
     public static void main(String[] args) {
