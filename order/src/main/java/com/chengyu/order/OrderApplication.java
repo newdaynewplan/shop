2a5dccff27a436637f0f9f9da50171704736e651
jiangchengyu <jiangchengyu@ly-sky.com>
2018-09-26 15:06:48 +0800

    加入聚合工程 turbine。

--- order/src/main/java/com/chengyu/order/OrderApplication.java
+++ order/src/main/java/com/chengyu/order/OrderApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -13,6 +15,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @MapperScan("com.chengyu.order.dao")
 @EnableTransactionManagement  // 开启事务
+@EnableHystrix  // 加上@EnableHystrix注解开启断路器
+@EnableHystrixDashboard  // 加上@EnableHystrixDashboard注解，开启HystrixDashboard
 public class OrderApplication {
 
     public static void main(String[] args) {
---
+++ turbine/README.md
@@ -0,0 +1,2 @@
+
+- http://192.168.6.235:8088/turbine.stream
